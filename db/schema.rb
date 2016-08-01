3dc72883684e5ddc3dc1f1216b64624ac1de4644
Josef Zacek <info@josefzacek.com>
2016-08-01 19:54:12 +0100

    migrating Chatroom scaffold
    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160731192059) do
+ActiveRecord::Schema.define(version: 20160801184103) do
+
+  create_table "chatrooms", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "email",                  default: "", null: false
