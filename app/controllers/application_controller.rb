c57519a77ee7aeac939995b98d88860223c8278f
Josef Zacek <info@josefzacek.com>
2016-08-06 23:18:30 +0100

    tell app to use "layout_for_devise" if devise_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,20 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  layout :devise_layout
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  def devise_layout
+    if devise_controller?
+      'layout_for_devise'
+    else
+      'application'
+    end
   end
 end
