b361a5e11ab735607fa165cf4ef2b780e87509b6
Josef Zacek <info@josefzacek.com>
2016-08-07 22:35:57 +0100

    create method to allow current user to join chatroom

--- app/controllers/chatroom_users_controller.rb
+++ app/controllers/chatroom_users_controller.rb
@@ -1,6 +1,11 @@
 class ChatroomUsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_chatroom
+
+  def create
+    @chatroom_user = @chatroom.chatroom_users.where(user_id: current_user.id).first_or_create
+    redirect_to @chatroom
+  end
   private
 
   def set_chatroom
