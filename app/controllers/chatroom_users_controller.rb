f5af8885c63d5ced55245ab5844d7cb48966e7c2
Josef Zacek <info@josefzacek.com>
2016-08-07 22:35:06 +0100

    use before_action to find chatroom

--- app/controllers/chatroom_users_controller.rb
+++ app/controllers/chatroom_users_controller.rb
@@ -1,3 +1,9 @@
 class ChatroomUsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_chatroom
+  private
+
+  def set_chatroom
+    @chatroom = Chatroom.find(params[:chatroom_id])
+  end
 end
