31894ca50c0a5a50e13e0a42bf66d60203145e39
Josef Zacek <info@josefzacek.com>
2016-08-14 21:28:34 +0100

    add before_action :set_chatroom

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,7 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_chatroom
+  def set_chatroom
+    @chatroom = Chatroom.find(params[:chatroom_id])
+  end
 end
